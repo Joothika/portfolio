bcc5b80b8eb8e72c39fc8229c707fa3f8fb5144d
Joothika <jothikakanthuluvaseenivasan@gmail.com>
2024-09-01 14:21:56 +0530

    project section edited

--- src/utils/GridContainer.jsx
+++ src/utils/GridContainer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const GridContainer = ({ children, addCSS }) => {
   return (
-    <main className="sm:mx-10 my-4 md:my-0 md:mx-10 ">
+    <main className="mx-10 sm:mx-10 my-4 md:my-0 ">
       <ul className={`grid grid-rows-12 sm:grid-cols-12 xl:gap-y-0 ${addCSS}`}>
         {children}
       </ul>
